3f91740048c341bd98f61f766487954ce9f98c59
JohnMaster1999 <John.Master.1999@gmail.com>
2023-05-04 09:30:01 +0200

    Revert "Delete routes directory"
    This reverts commit edb2280003fafcacca232e1efd132556c03f8fc9.

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', 'Navegador@index')->name('index');
+Route::get('/peinados', 'Navegador@peinados')->name('peinados');
+Route::get('/informacion', 'Navegador@informacion')->name('informacion');
+Route::get('/citas', 'Navegador@citas')->name('citas');
+Route::get('/faqs', 'Navegador@faqs')->name('faqs');
+
+Route::post('/guardar-cita', 'CitaController@guardar');
+
+Route::get('/admin', 'Admin@admin')->name('admin');
+Route::get('/consultar', 'Admin@consultar')->name('consultar');
+Route::get('/anadir', 'Admin@anadir')->name('anadir');
+Route::get('/eliminar', 'Admin@eliminar')->name('eliminar');
+Route::get('/consCitas', 'Admin@consCitas')->name('consCitas');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
